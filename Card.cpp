2520ef03b9831a27cd0b8eb2d1664967b18b18e3
Pletnov Denis <pletnevd732@gmail.com>
2025-06-23 19:12:21 +0300

    Додано реалізацію сеттерів та геттерів

--- Card.cpp
+++ Card.cpp
@@ -36,14 +36,62 @@ void Card::showInfo()
 	cout << "Cash: " << cash << endl;
 }
 
+int Card::getNumber()
+{
+	return number;
+}
+
+const char* Card::getName()
+{
+	return name;
+}
+
+const char* Card::getExpiredDate()
+{
+	return expiredDate;
+}
+
+int Card::getPIN()
+{
+	return PIN;
+}
+
+double Card::getCash()
+{
+	return cash;
+}
+
 void Card::setPIN(int old_PIN, int new_PIN)
 {
+	if (PIN == old_PIN)
+		PIN = new_PIN;
+	else
+	{
+		cout << "Error\n";
+	}
 }
 
 void Card::setName(const char* new_name)
 {
+	if (strlen(new_name) < 3)
+		cout << "Too short name\n";
+	else {
+		delete[] name;
+		int size = strlen(new_name) + 1;
+		name = new char[size];
+		strcpy_s(name, size, new_name);
+	}
+
 }
 
 void Card::setExpiredDate(const char* new_ExpiredDate)
 {
+	if (strlen(new_ExpiredDate) != 5)
+	{
+		cout << "Invaild data\n";
+	}
+	else
+	{
+		strcpy_s(expiredDate, 6, new_ExpiredDate);
+	}
 }
