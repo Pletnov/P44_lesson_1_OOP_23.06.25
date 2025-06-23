7262e4648533e1dbabb0039888bd78b8aa172518
Pletnov Denis <pletnevd732@gmail.com>
2025-06-23 18:51:20 +0300

    Додано реалізацію

--- Card.cpp
+++ Card.cpp
@@ -2,16 +2,36 @@
 
 Card::Card()
 {
+	number = rand() + rand() * 10000;
+	name = new char[10] {"Undefined"};
+	strcpy_s(expiredDate, 6, "01.35");
+	PIN = rand() % 800 + 100;
+	cash = 0;
 }
 
 Card::Card(const char* user_name)
 {
+	number = rand() + rand() * 10000;
+	strcpy_s(expiredDate, 6, "01.35");
+	PIN = rand() % 800 + 100;
+	cash = 0;
+
+	int size = strlen(user_name) + 1;
+	name = new char[size];
+	strcpy_s(name, size, user_name);
 }
 
 Card::~Card()
 {
+	cout << "Close the card" << endl;
+	delete[] name;
 }
 
 void Card::showInfo()
 {
+	cout << "Number card: " << number << endl;
+	cout << "Name: " << name << endl;
+	cout << "Exp date: " << expiredDate << endl;
+	cout << "PIN: " << PIN << endl;
+	cout << "Cash: " << cash << endl;
 }
